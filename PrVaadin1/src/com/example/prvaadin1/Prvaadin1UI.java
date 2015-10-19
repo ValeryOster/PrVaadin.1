7389484d4ad598661126d02024d1085f84d9b4f1
Valery <oster_valera@hotmail.com>
2015-10-19 17:10:31 +0200

    narabote dopolnil

--- PrVaadin1/src/com/example/prvaadin1/Prvaadin1UI.java
+++ PrVaadin1/src/com/example/prvaadin1/Prvaadin1UI.java
@@ -1,32 +1,42 @@
 package com.example.prvaadin1;
 
+import java.io.File;
+
 import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.data.util.FilesystemContainer;
 import com.vaadin.event.dd.DragAndDropEvent;
 import com.vaadin.event.dd.DropHandler;
 import com.vaadin.event.dd.acceptcriteria.AcceptAll;
 import com.vaadin.event.dd.acceptcriteria.AcceptCriterion;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinService;
 import com.vaadin.server.VaadinServlet;
-
+import com.vaadin.server.VaadinSession;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.ComponentContainer;
 import com.vaadin.ui.DragAndDropWrapper;
 import com.vaadin.ui.DragAndDropWrapper.DragStartMode;
 import com.vaadin.ui.HorizontalSplitPanel;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.Tree;
+import com.vaadin.ui.TreeTable;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
 @SuppressWarnings("serial")
 @Theme("prvaadin1")
 public class Prvaadin1UI extends UI {
-
+    private TreeTable treetable;
+    private File currentPath;
+    
+    
+    
     HorizontalSplitPanel splitPanel;
     DragAndDropWrapper wrapperA;
     DragAndDropWrapper wrapperB;
@@ -127,7 +137,21 @@ public class Prvaadin1UI extends UI {
             }
 
         });
+        
 
+
+//filesystem build
+        treetable = new TreeTable("File System");
+        treetable.setSelectable(true);
+        treetable.setColumnCollapsingAllowed(true);
+        treetable.setColumnReorderingAllowed(true);
+        treetable.setSizeFull();
+
+        
+//ende
+        
+        
+        
         leftPanel.setSizeFull();
         rightPanel.setSizeFull();
         leftPanelWrapper.setSizeFull();
@@ -142,11 +166,65 @@ public class Prvaadin1UI extends UI {
 
         VerticalLayout layout = new VerticalLayout();
         layout.addComponent(dragMode);
+        layout.addComponent(treetable);
         layout.addComponent(splitPanel);
         layout.setSizeFull();
 
         this.setContent(layout);
         this.setSizeFull();
 	}
+	
+	
+	private void updateFileTree(File sourcePath) {
 
+	    FilesystemContainer currentFileSystem = new FilesystemContainer(sourcePath);
+	    currentFileSystem.setRecursive(false); 
+
+	    treetable.setContainerDataSource(currentFileSystem);
+	    treetable.setItemIconPropertyId("Icon");
+	    treetable.setVisibleColumns(new Object[]{"Name", "Size", "Last Modified"}); 
+	}
+
+	private void getDefaultDirectory() {
+	    UI ui = MyVaadinUI.getCurrent();
+	    VaadinSession session = ui.getSession();
+	    VaadinService service = session.getService();
+	    currentPath = service.getBaseDirectory();
+	}
+
+	private void initAll(VerticalLayout layout) {
+	    initFileTree(layout);
+	    getDefaultDirectory();
+	    updateFileTree(currentPath);
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
